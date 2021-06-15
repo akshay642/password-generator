e04be1feb6f5fe496f45484580b07d108fb136fe
akshay642 <84628260+akshay642@users.noreply.github.com>
2021-06-15 22:22:45 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,25 @@
+
+
+from colored import stylize, fg
+import string, random
+
+# decoration
+print(stylize("\n---- | Password generator | ----\n", fg("red")))
+
+# variables
+chars = "".join(string.printable.split())
+password = ""
+
+# user interaction
+number_of_pws = int(input("Number of passwords: "))
+len_of_pws = int(input("Length of each password: "))
+
+# output
+print("\nGenerating passwords...\n")
+
+for _ in range(number_of_pws):
+    for _ in range(len_of_pws):
+        password += random.choice(chars)
+    print(">>> " + password)
+    password = ""
+print()
